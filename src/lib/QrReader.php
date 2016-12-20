68c5f772f2b7477745390d6863c272869dd1d899
VahiD Davari <info.davari@gmail.com>
2016-12-20 15:23:14 +0330

    Fix code style formatted and add boolean cast to isBase64 method

--- src/lib/QrReader.php
+++ src/lib/QrReader.php
@@ -1,49 +1,49 @@
 <?php namespace Libern\QRCodeReader\lib;
 
-include_once ('Reader.php');
-require_once ('BinaryBitmap.php');
-require_once ('common/detector/MathUtils.php');
-require_once ('common/BitMatrix.php');
-require_once ('common/BitSource.php');
-require_once ('common/BitArray.php');
-require_once ('BinaryBitmap.php');
-include_once ('LuminanceSource.php');
-include_once ('GDLuminanceSource.php');
-include_once ('IMagickLuminanceSource.php');
-include_once ('common/customFunctions.php');
-include_once ('common/PerspectiveTransform.php');
-include_once ('common/GridSampler.php');
-include_once ('common/DefaultGridSampler.php');
-include_once ('common/DetectorResult.php');
-require_once ('common/reedsolomon/GenericGFPoly.php');
-require_once ('common/reedsolomon/GenericGF.php');
-include_once ('common/reedsolomon/ReedSolomonDecoder.php');
-include_once ('common/reedsolomon/ReedSolomonException.php');
+include_once('Reader.php');
+require_once('BinaryBitmap.php');
+require_once('common/detector/MathUtils.php');
+require_once('common/BitMatrix.php');
+require_once('common/BitSource.php');
+require_once('common/BitArray.php');
+require_once('BinaryBitmap.php');
+include_once('LuminanceSource.php');
+include_once('GDLuminanceSource.php');
+include_once('IMagickLuminanceSource.php');
+include_once('common/customFunctions.php');
+include_once('common/PerspectiveTransform.php');
+include_once('common/GridSampler.php');
+include_once('common/DefaultGridSampler.php');
+include_once('common/DetectorResult.php');
+require_once('common/reedsolomon/GenericGFPoly.php');
+require_once('common/reedsolomon/GenericGF.php');
+include_once('common/reedsolomon/ReedSolomonDecoder.php');
+include_once('common/reedsolomon/ReedSolomonException.php');
 
-include_once ('qrcode/decoder/Decoder.php');
-include_once ('ReaderException.php');
-include_once ('NotFoundException.php');
-include_once ('FormatException.php');
-include_once ('ChecksumException.php');
-include_once ('qrcode/detector/FinderPatternInfo.php');
-include_once ('qrcode/detector/FinderPatternFinder.php');
-include_once ('ResultPoint.php');
-include_once ('qrcode/detector/FinderPattern.php');
-include_once ('qrcode/detector/AlignmentPatternFinder.php');
-include_once ('qrcode/detector/AlignmentPattern.php');
-include_once ('qrcode/decoder/Version.php');
-include_once ('qrcode/decoder/BitMatrixParser.php');
-include_once ('qrcode/decoder/FormatInformation.php');
-include_once ('qrcode/decoder/ErrorCorrectionLevel.php');
-include_once ('qrcode/decoder/DataMask.php');
-include_once ('qrcode/decoder/DataBlock.php');
-include_once ('qrcode/decoder/DecodedBitStreamParser.php');
-include_once ('qrcode/decoder/Mode.php');
-include_once ('common/DecoderResult.php');
-include_once ('Result.php');
-include_once ('Binarizer.php');
-include_once ('common/GlobalHistogramBinarizer.php');
-include_once ('common/HybridBinarizer.php');
+include_once('qrcode/decoder/Decoder.php');
+include_once('ReaderException.php');
+include_once('NotFoundException.php');
+include_once('FormatException.php');
+include_once('ChecksumException.php');
+include_once('qrcode/detector/FinderPatternInfo.php');
+include_once('qrcode/detector/FinderPatternFinder.php');
+include_once('ResultPoint.php');
+include_once('qrcode/detector/FinderPattern.php');
+include_once('qrcode/detector/AlignmentPatternFinder.php');
+include_once('qrcode/detector/AlignmentPattern.php');
+include_once('qrcode/decoder/Version.php');
+include_once('qrcode/decoder/BitMatrixParser.php');
+include_once('qrcode/decoder/FormatInformation.php');
+include_once('qrcode/decoder/ErrorCorrectionLevel.php');
+include_once('qrcode/decoder/DataMask.php');
+include_once('qrcode/decoder/DataBlock.php');
+include_once('qrcode/decoder/DecodedBitStreamParser.php');
+include_once('qrcode/decoder/Mode.php');
+include_once('common/DecoderResult.php');
+include_once('Result.php');
+include_once('Binarizer.php');
+include_once('common/GlobalHistogramBinarizer.php');
+include_once('common/HybridBinarizer.php');
 
 
 final class QrReader
@@ -64,13 +64,13 @@ final class QrReader
                 $im = imagecreatefromstring($imageStream);
                 $source = new \Zxing\GDLuminanceSource($im, $imageSize[0], $imageSize[1]);
             } else {
-                if(extension_loaded('imagick')) {
+                if (extension_loaded('imagick')) {
                     $im = new \Imagick();
                     $im->readImage($filename);
                     $width = $im->getImageWidth();
                     $height = $im->getImageHeight();
                     $source = new \Zxing\IMagickLuminanceSource($im, $width, $height);
-                }else {
+                } else {
                     $image = file_get_contents($filename);
                     $sizes = getimagesize($filename);
                     $width = $sizes[0];
@@ -86,20 +86,20 @@ final class QrReader
             $reader = new \Zxing\Qrcode\QRCodeReader();
 
             $this->result = $reader->decode($bitmap);
-        }catch (\Zxing\NotFoundException $er){
+        } catch (\Zxing\NotFoundException $er) {
             $this->result = false;
-        }catch( \Zxing\FormatException $er){
+        } catch (\Zxing\FormatException $er) {
             $this->result = false;
-        }catch( \Zxing\ChecksumException $er){
+        } catch (\Zxing\ChecksumException $er) {
             $this->result = false;
         }
     }
 
     public function text()
     {
-        if(method_exists($this->result,'toString')) {
-            return  ($this->result->toString());
-        }else{
+        if (method_exists($this->result, 'toString')) {
+            return ($this->result->toString());
+        } else {
             return $this->result;
         }
     }
@@ -109,8 +109,9 @@ final class QrReader
      * @param $string
      * @return bool
      */
-    private function isBase64 ($string) {
-        return preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $string);
+    private function isBase64($string)
+    {
+        return (bool)preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $string);
     }
 }
 
