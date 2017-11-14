34745eb884e394a4cdbebe49a80ae319c79d87ad
Anne Douwe Bouma <annedouwebouma@gmail.com>
2017-11-14 21:50:15 +0100

    Fix for direct use of PHP temp path via file upload

--- src/lib/QrReader.php
+++ src/lib/QrReader.php
@@ -111,7 +111,7 @@ final class QrReader
      */
     private function isBase64($string)
     {
-        return (bool)preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $string);
+        return (bool)preg_match('/^[a-zA-Z0-9\r\n+]*={0,2}$/', $string);
     }
 }
 
